82fdc717f3e2d70b76d1d0848e6dae6e778bb5b2
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-02-24 11:52:11 -0300

    Refactoring Typeorm Repositories

--- aulas/api/src/controllers/UserController.ts
+++ aulas/api/src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -28,4 +28,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
