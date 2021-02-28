d48337fd8d9f02dbcd3a703705f83f0c48cdbe1e
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-02-28 12:23:29 -0300

    Creating the API documentation with swagger

--- aulas/api/src/app.ts
+++ aulas/api/src/app.ts
@@ -7,7 +7,10 @@ import { router } from "./routes";
 
 createConnection();
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(router);
 
